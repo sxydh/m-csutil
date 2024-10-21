150b67c1767c9ff90c529b7c28461124a68e1a4d
sxydh <sxydhgg@gmail.com>
2024-10-21 22:21:12 +0800

    [UPDATE]

---
+++ MCSUtil.Test/CoreTest/NetHelperTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MNetUtil.Core;
+
+namespace MNetUtil.Test.CoreTest
+{
+    [TestClass]
+    public class FileServerTest
+    {
+        [TestMethod]
+        public void TestNew()
+        {
+            var fileServer = new FileServer(8080, "");
+            fileServer.Start().Wait(TimeSpan.FromSeconds(1));
+        }
+
+        [TestMethod]
+        public void TestGetContentType()
+        {
+            var contentType = FileServer.GetContentType(".js");
+            Assert.IsNotNull(contentType);
+        }
+
+        [TestMethod]
+        public void TestSubFileServer()
+        {
+            var subFileServer = new SubFileServer(8080, "");
+            subFileServer.Start().Wait(TimeSpan.FromSeconds(1));
+        }
+    }
+
+    internal class SubFileServer : FileServer
+    {
+        internal SubFileServer(int port, string root) : base(port, root)
+        {
+        }
+    }
+}
