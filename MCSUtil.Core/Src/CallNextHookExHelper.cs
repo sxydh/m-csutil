cba058bd7938f039d4b3c9c1abf3eadbf11614cc
sxydh <sxydhgg@gmail.com>
2024-10-24 19:01:59 +0800

    [UPDATE]

---
+++ MCSUtil.Core/Src/CallNextHookExHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace MCSUtil.Core
+{
+    public static class CallNextHookExHelper
+    {
+        public static IntPtr Call(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            return CallNextHookEx(hhk, nCode, wParam, lParam);
+        }
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+    }
+}
