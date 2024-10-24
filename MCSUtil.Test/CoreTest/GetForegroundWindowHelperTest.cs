c077c10646cfad757656065fb091e46c28ab2a49
sxydh <sxydhgg@gmail.com>
2024-10-24 18:30:33 +0800

    [UPDATE]

---
+++ MCSUtil.Test/CoreTest/GetForegroundWindowHelperTest.cs
@@ -0,0 +1,16 @@
+﻿using MCSUtil.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MCSUtil.Test.CoreTest
+{
+    [TestClass]
+    public class GetForegroundWindowHelperTest
+    {
+        [TestMethod]
+        public void TestGet()
+        {
+            var ptr = GetForegroundWindowHelper.Get();
+            Assert.IsNotNull(ptr);
+        }
+    }
+}
