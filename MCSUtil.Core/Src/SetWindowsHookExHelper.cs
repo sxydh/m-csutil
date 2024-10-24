10cc8e6f78da720570a2343a9a06359bd2f9e0a2
sxydh <sxydhgg@gmail.com>
2024-10-24 19:32:44 +0800

    [UPDATE]

--- MCSUtil.Core/Src/SetWindowsHookExHelper.cs
+++ MCSUtil.Core/Src/SetWindowsHookExHelper.cs
@@ -7,14 +7,22 @@ namespace MCSUtil.Core
     {
         private const int WH_KEYBOARD_LL = 13;
 
-        public static void HookKeyboard(HOOKPROC lpfn)
+        public static IntPtr HookKeyboard(HOOKPROC lpfn)
         {
-            SetWindowsHookEx(WH_KEYBOARD_LL, lpfn, IntPtr.Zero, 0);
+            return SetWindowsHookEx(WH_KEYBOARD_LL, lpfn, IntPtr.Zero, 0);
+        }
+
+        public static bool UnHookKeyboard(IntPtr hhk)
+        {
+            return UnhookWindowsHookEx(hhk);
         }
 
         public delegate IntPtr HOOKPROC(int nCode, IntPtr wParam, IntPtr lParam);
 
         [DllImport("user32.dll")]
         private static extern IntPtr SetWindowsHookEx(int idHook, HOOKPROC lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll")]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
     }
 }
---
+++ MCSUtil.Test/CoreTest/SetWindowsHookExHelperTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using MCSUtil.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MCSUtil.Test.CoreTest
+{
+    [TestClass]
+    public class SetWindowsHookExHelperTest
+    {
+        [TestMethod]
+        public void TestHookKeyboard()
+        {
+            var hhk = SetWindowsHookExHelper.HookKeyboard(OnKeyboard);
+            Assert.IsNotNull(hhk);
+        }
+
+        private static IntPtr OnKeyboard(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            return CallNextHookExHelper.Call(IntPtr.Zero, nCode, wParam, lParam);
+        }
+    }
+}
