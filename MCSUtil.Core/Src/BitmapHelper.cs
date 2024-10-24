3521463fa8e9a56fde3048798c4a59b22869c076
sxydh <sxydhgg@gmail.com>
2024-10-25 00:33:42 +0800

    [UPDATE]

--- MCSUtil.Core/Src/BitmapHelper.cs
+++ MCSUtil.Core/Src/BitmapHelper.cs
@@ -1,13 +1,36 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
 
 namespace MCSUtil.Core
 {
     public static class BitmapHelper
     {
-        public static void Compress(string sourceFileName, string destFileName, int quality)
+        public static bool CompressJpeg(string sourceFileName, string destFileName, int quality)
+        {
+            return Compress(sourceFileName, destFileName, quality, ImageFormat.Jpeg.Guid);
+        }
+
+        public static bool CompressPng(string sourceFileName, string destFileName, int quality)
+        {
+            return Compress(sourceFileName, destFileName, quality, ImageFormat.Png.Guid);
+        }
+
+        private static bool Compress(string sourceFileName, string destFileName, int quality, Guid formatId)
         {
             using (var bitmap = new Bitmap(sourceFileName))
             {
+                var jpgCodecInfo = ImageCodecInfo.GetImageEncoders().FirstOrDefault(e => e.FormatID == formatId);
+                if (jpgCodecInfo == null)
+                {
+                    return false;
+                }
+
+                var encoderParams = new EncoderParameters(1);
+                encoderParams.Param[0] = new EncoderParameter(Encoder.Quality, quality);
+                bitmap.Save(destFileName, jpgCodecInfo, encoderParams);
+                return true;
             }
         }
     }
---
+++ MCSUtil.Test/CoreTest/BitmapHelperTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using MCSUtil.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MCSUtil.Test.CoreTest
+{
+    [TestClass]
+    public class BitmapHelperTest
+    {
+        [TestMethod]
+        public void TestCompressJpeg()
+        {
+            var source = Directory.GetCurrentDirectory();
+            var sourceFileName = Path.Combine(source, $"{typeof(BitmapHelperTest).FullName}.jpeg");
+            var destFileName = Path.Combine(source, $"{typeof(BitmapHelperTest).FullName}.{DateTimeHelper.Timestamp()}.jpeg");
+            var jpeg = new Bitmap(100, 100);
+            jpeg.Save(sourceFileName, ImageFormat.Jpeg);
+
+            var success = BitmapHelper.CompressJpeg(sourceFileName, destFileName, 50);
+            Assert.IsTrue(success);
+
+            File.Delete(sourceFileName);
+            File.Delete(destFileName);
+        }
+    }
+}
