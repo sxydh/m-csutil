0fab86af5494edcac26fdfbc25e5d37b8bae4ad8
sxydh <sxydhgg@gmail.com>
2024-10-25 00:00:56 +0800

    [UPDATE]

---
+++ MCSUtil.Core/Src/BitmapHelper.cs
@@ -0,0 +1,14 @@
+﻿using System.Drawing;
+
+namespace MCSUtil.Core
+{
+    public static class BitmapHelper
+    {
+        public static void Compress(string sourceFileName, string destFileName, int quality)
+        {
+            using (var bitmap = new Bitmap(sourceFileName))
+            {
+            }
+        }
+    }
+}
