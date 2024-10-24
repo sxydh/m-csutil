9e970e319115168673ff487679bd7dd1df19b296
sxydh <sxydhgg@gmail.com>
2024-10-24 10:53:26 +0800

    [UPDATE]

---
+++ MCSUtil.Test/CoreTest/TcpListenerHelperTest.cs
@@ -0,0 +1,16 @@
+﻿using MCSUtil.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MCSUtil.Test.CoreTest
+{
+    [TestClass]
+    public class TcpListenerHelperTest
+    {
+        [TestMethod]
+        public void TestIsPortInUse()
+        {
+            var isPortInUse = TcpListenerHelper.IsPortInUse(8080);
+            Assert.IsNotNull(isPortInUse);
+        }
+    }
+}
