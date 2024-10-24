e7d0a77c92e42cf9b63ab1c99e924acca9341ab2
sxydh <sxydhgg@gmail.com>
2024-10-24 11:42:53 +0800

    [UPDATE]

---
+++ MCSUtil.Test/CoreTest/ProcessHelperTest.cs
@@ -0,0 +1,16 @@
+﻿using MCSUtil.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MCSUtil.Test.CoreTest
+{
+    [TestClass]
+    public class ProcessHelperTest
+    {
+        [TestMethod]
+        public void TestStart()
+        {
+            var processId = ProcessHelper.Start("cmd");
+            Assert.IsNotNull(processId);
+        }
+    }
+}
