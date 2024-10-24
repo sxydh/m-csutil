9b56d886e2b1d428139caac458fd7338b4e7717a
sxydh <sxydhgg@gmail.com>
2024-10-24 18:34:57 +0800

    [UPDATE]

--- MCSUtil.Core/Src/SetWindowPosHelper.cs
+++ MCSUtil.Core/Src/SetWindowPosHelper.cs
@@ -5,8 +5,19 @@ namespace MCSUtil.Core
 {
     public static class SetWindowPosHelper
     {
-        public static void TopWindow()
+        private static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);
+        private static readonly IntPtr HWND_NOTOPMOST = new IntPtr(-2);
+        private const int SWP_NOSIZE = 0x0001;
+        private const int SWP_NOMOVE = 0x0002;
+
+        public static bool TopWindow(IntPtr hWnd)
         {
+            return SetWindowPos(hWnd, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOSIZE | SWP_NOMOVE);
+        }
+
+        public static bool UnTopWindow(IntPtr hWnd)
+        {
+            return SetWindowPos(hWnd, HWND_NOTOPMOST, 0, 0, 0, 0, SWP_NOSIZE | SWP_NOMOVE);
         }
 
         [DllImport("user32.dll")]
---
+++ MCSUtil.Test/CoreTest/SetWindowPosHelperTest.cs
@@ -0,0 +1,29 @@
+﻿using MCSUtil.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MCSUtil.Test.CoreTest
+{
+    [TestClass]
+    public class SetWindowPosHelperTest
+    {
+        [TestMethod]
+        public void TestTopWindow()
+        {
+            var ptr = GetForegroundWindowHelper.Get();
+            Assert.IsNotNull(ptr);
+
+            var success = SetWindowPosHelper.TopWindow(ptr);
+            Assert.IsTrue(success);
+        }
+
+        [TestMethod]
+        public void TestUnTopWindow()
+        {
+            var ptr = GetForegroundWindowHelper.Get();
+            Assert.IsNotNull(ptr);
+
+            var success = SetWindowPosHelper.UnTopWindow(ptr);
+            Assert.IsTrue(success);
+        }
+    }
+}
