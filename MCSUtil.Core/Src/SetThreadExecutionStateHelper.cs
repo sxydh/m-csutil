e046fda61693959df82c6501c1941b5c183da596
sxydh <sxydhgg@gmail.com>
2024-10-24 23:55:39 +0800

    [UPDATE]

---
+++ MCSUtil.Core/Src/SetThreadExecutionStateHelper.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.InteropServices;
+
+namespace MCSUtil.Core
+{
+    public static class SetThreadExecutionStateHelper
+    {
+        private const uint ES_CONTINUOUS = 0x80000000;
+        private const uint ES_DISPLAY_REQUIRED = 0x00000002;
+        private const uint ES_SYSTEM_REQUIRED = 0x00000001;
+
+        public static uint KeepAwake()
+        {
+            return SetThreadExecutionState(ES_CONTINUOUS | ES_DISPLAY_REQUIRED | ES_SYSTEM_REQUIRED);
+        }
+
+        public static uint ResetAwake()
+        {
+            return SetThreadExecutionState(ES_CONTINUOUS);
+        }
+
+        [DllImport("kernel32.dll")]
+        private static extern uint SetThreadExecutionState(uint esFlags);
+    }
+}
---
+++ MCSUtil.Test/CoreTest/SetThreadExecutionStateHelperTest.cs
@@ -0,0 +1,23 @@
+﻿using MCSUtil.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MCSUtil.Test.CoreTest
+{
+    [TestClass]
+    public class SetThreadExecutionStateHelperTest
+    {
+        [TestMethod]
+        public void TestKeepAwake()
+        {
+            var state = SetThreadExecutionStateHelper.KeepAwake();
+            Assert.IsNotNull(state);
+        }
+
+        [TestMethod]
+        public void TestResetAwake()
+        {
+            var state = SetThreadExecutionStateHelper.ResetAwake();
+            Assert.IsNotNull(state);
+        }
+    }
+}
