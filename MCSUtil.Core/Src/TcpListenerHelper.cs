f381b066b20b5af616e59987d9f8f3aea96b6088
sxydh <sxydhgg@gmail.com>
2024-10-24 10:39:54 +0800

    [UPDATE]

---
+++ MCSUtil.Core/Src/TcpListenerHelper.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace MCSUtil.Core
+{
+    public class TcpListenerHelper
+    {
+        public static bool IsPortInUse(int port)
+        {
+            TcpListener listener = null;
+            try
+            {
+                listener = new TcpListener(IPAddress.Any, port);
+                listener.Start();
+                return true;
+            }
+            catch (SocketException)
+            {
+                return false;
+            }
+            finally
+            {
+                listener?.Stop();
+            }
+        }
+    }
+}
