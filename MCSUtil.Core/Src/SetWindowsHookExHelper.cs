5b5cb49cf40261d2919b3b9ac0d0bd833fe3fdd7
sxydh <sxydhgg@gmail.com>
2024-10-24 18:54:10 +0800

    [UPDATE]

---
+++ MCSUtil.Core/Src/SetWindowsHookExHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace MCSUtil.Core
+{
+    public static class SetWindowsHookExHelper
+    {
+        private const int WH_KEYBOARD_LL = 13;
+
+        public static void HookKeyboard(HOOKPROC lpfn)
+        {
+            SetWindowsHookEx(WH_KEYBOARD_LL, lpfn, IntPtr.Zero, 0);
+        }
+
+        public delegate IntPtr HOOKPROC(int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr SetWindowsHookEx(int idHook, HOOKPROC lpfn, IntPtr hMod, uint dwThreadId);
+    }
+}
