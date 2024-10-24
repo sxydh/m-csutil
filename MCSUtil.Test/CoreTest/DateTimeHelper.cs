a05da4e123d3e68fd933c860b125765578cecc2a
sxydh <sxydhgg@gmail.com>
2024-10-25 00:29:54 +0800

    [UPDATE]

---
+++ MCSUtil.Test/CoreTest/DateTimeHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MCSUtil.Test.CoreTest
+{
+    public static class DateTimeHelper
+    {
+        public static long Timestamp()
+        {
+            return DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+        }
+    }
+}
---
+++ MCSUtil.Test/CoreTest/DateTimeHelperTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MCSUtil.Test.CoreTest
+{
+    [TestClass]
+    public class DateTimeHelperTest
+    {
+        [TestMethod]
+        public void TestTimestamp()
+        {
+            var timestamp = DateTimeHelper.Timestamp();
+            Assert.IsNotNull(timestamp);
+        }
+    }
+}
