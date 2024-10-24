cacaa50fa041deaceb2cb873117fc94ec3dee66e
sxydh <sxydhgg@gmail.com>
2024-10-24 18:28:51 +0800

    [UPDATE]

---
+++ MCSUtil.Core/Src/GetForegroundWindowHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace MCSUtil.Core
+{
+    public static class GetForegroundWindowHelper
+    {
+        public static IntPtr Get()
+        {
+            return GetForegroundWindow();
+        }
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+    }
+}
