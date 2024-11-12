cdefacd7befc260f338d07539b5217db1aa85f74
sxydh <sxydhgg@gmail.com>
2024-11-12 12:13:51 +0800

    [UPDATE]

---
+++ MCSUtil.Core/Src/DateTimeHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MCSUtil.Core
+{
+    public static class DateTimeHelper
+    {
+        public static long Timestamp()
+        {
+            return DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+        }
+    }
+}
